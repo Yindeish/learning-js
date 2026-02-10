5e81135675996ea9ed6b3bf630d8b2e4a1a6aaf4
Yindeish <adeshinaadam03@gmail.com>
2026-02-10 08:10:33 +0100

    added sample code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+'use client'
+
 
 export default function Home() {
+
+  console.log('first')
+
   return (
-    <div className="text-4xl text-black"></div>
+    <div className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[red] to-[green]">
+      LEARNING JAVASCRIPT/TYPESCRIPT
+    </div>
   );
 }
