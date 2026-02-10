f666f696c29f1b8050c335df375c8c54b5ce77f1
Yindeish <adeshinaadam03@gmail.com>
2026-02-10 09:15:41 +0100

    on adding and deleting items

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,23 @@
 
 export default function Home() {
 
-  console.log('first')
+  // Declaration
+  const arrays: number[] = [];
+
+  arrays.push(1)
+
+  arrays.push(2.5) //float
+
+  arrays.push(0)
+  // 4, 5
+  console.log(arrays.push(4, 5))
+
+  // arrays.pop()
+
+  console.log(arrays.pop())
 
   return (
-    <div className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[red] to-[green]">
+    <div className="w-screen h-screen bg-white flex items-center justify-center text-center text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[green] to-[black]">
       LEARNING JAVASCRIPT/TYPESCRIPT
     </div>
   );
