d49fef904edd1643405674f4894dcbf144c3aa57
Yindeish <adeshinaadam03@gmail.com>
2026-02-25 08:15:09 +0100

    switching from objects to object

--- app/declaration/page.tsx
+++ app/declaration/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Montserrat } from "next/font/google";
+import { useState } from "react";
 
 const mont = Montserrat({
     weight: ['100', '200', '900'],
@@ -8,13 +9,33 @@ const mont = Montserrat({
 
 
 export default function Home() {
+    const variable = 'value';
 
-   
+    // key-value pairs
+    // object is like a human being
+    // name -> details
+    // hair (black or green etc)
+    // speak -> action
+
+    const user = {
+        // we can declare (define) variabes inside this object
+        // variable declaration
+        name: 'Rehab', // string
+        age: 20, // number
+        graduated: true, // boolean,
+        speak: () => {
+            console.log('speaking..')
+        }
+    }
+
+    console.log(user, 'user')
+
+
     // JSX/TSX starts here
     return (
         <div className="w-screen h-screen bg-white flex flex-col items-center justify-center">
             <div className={`text-center text-[70px] font-black text-transparent bg-clip-text bg-gradient-to-r from-[green] to-[black] ${mont.className}`}>
-                Javascript Objects
+                Objects Declaration
             </div>
             <div className="text-2xl text-black flex flex-col gap-1">
 
---
+++ objects.md
@@ -0,0 +1,15 @@
+## Objects
+
+# Objects Declaration
+1. inside object varaible declaration
+
+
+# Extracting values and Destructuring
+
+# Cloning (Copying) objects
+
+# Updating Data
+
+# Essential Object Methods
+1. keys methods
+2. values method
