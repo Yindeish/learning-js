b877ddba4599eb5c69338598bd3242f922699806
Yindeish <adeshinaadam03@gmail.com>
2026-03-10 09:56:41 +0100

    on basic types and custom typs

--- app/primitive-vs-reference2/page.tsx
+++ app/primitive-vs-reference2/page.tsx
@@ -8,7 +8,100 @@ const mont = Montserrat({
 })
 
 
+// Basic types
+// string
+// number
+// boolean
+// Function
+// () => {}
+// Array
+// []
+// object
+// {}
+// any
+// never
+// unknown
+
+// Custom types or User-defined type
+
+// using the type keyboard
+
+// const variableName = 'value'
+type typeName = string;
+
+// type myType = boolean;
+
+// using the interface keyword
+
+// const car = {
+//     name: 'BMW',
+//     expired: false
+// }
+
+type carType = {
+    name: string
+    expired: boolean,
+    model: number
+}
+
+// interface carType 
+
+// interface objecType {
+//     value: number,
+//     name: string,
+//     isMarried: boolean,
+//     speak: Function,
+//     children: []
+// }
+
+
+// DO IT HERE
+type counter = {
+    value: number
+}
+// DO IT HERE
+
+
 export default function Home() {
+
+    const name = 'Rehab'
+
+    const counter1 = {
+        value: 30
+    }
+
+    const arrays = []
+
+
+    const updateObjectBy5 = (value: counter) => {
+
+    }
+
+    const car = {
+        expired: true,
+        model: 2019,
+        name: 'BMW'
+    }
+
+    updateObjectBy5(counter1)
+
+
+
+
+
+    // const counter1 = {
+    //     value: 30
+    // }
+
+    // counter1.value = 40;
+
+
+    // const counter2 = counter1; // 40
+    // const counter3 = counter2; // 40
+
+    // counter3.value = 50;
+
+
 
 
 
