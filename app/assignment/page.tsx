7053f53c9a8cf85b6e15fcf9459d568acc922d8c
Yindeish <adeshinaadam03@gmail.com>
2026-03-14 13:05:49 +0100

    made corrections for assignments 010 and 011

--- app/assignment/page.tsx
+++ app/assignment/page.tsx
@@ -8,7 +8,7 @@ export default function Assignment() {
 //type TypeName = value;
 //type TypeName = value1 | value2 | value3;
 
-//1. Student Management System
+//1. Student Management System ✅
 type Coures = {
   courseCode: string;
   courseTitle: string;
@@ -21,7 +21,7 @@ type student = {
   fullName: string;
   age: number;
   email: string;
-  Coures: Coures[];
+  coures: Coures[];
 };
 
 //Student Management System
@@ -35,14 +35,15 @@ type User = {
 
 //2. Crypto Portfolio Tracker
 
-type Chain = "Ethereum" | "Bitcoin" | "Solana" | "Polygon";
+type Chain = "Ethereum" | "Bitcoin" | "Solana" | "Polygon"; // types seprated by OR ( | )
 
 type portfolio = {
   assetName: string;
   symbol: string;
   balance: number;
   priceUsd: number;
-  chain: string;
+  chain: Chain;
+  // chain: "Ethereum" | "Bitcoin" | "Solana" | "Polygon"
 };
 
 //2. Crypto Portfolio Tracker
@@ -55,25 +56,38 @@ type Message = {
   senderId: number;
   receiverId: number;
   content: string;
-  attachments: string;
-  timestamp: number;
+  // attachments: string;
+  attachments?: Attachment[] //Array of attachments or collections of attachments // ? mark makes it optional
+  // timestamp: number;
+  timestamp: Date,
+  dateOfBirth: Date
 };
+
+type Attachment = {
+  fileName: string
+  fileSize: number
+  fileType: string
+}
 
 //3. Messaging Application
 //E-commerce Order System
 
 type order = {
   orderId: number;
-  customerName: string;
-  products: string;
-  totalPrice: number;
-  orderStatus: boolean;
-  timestamp: number;
+  customerName: string
+  // products: string;
+  products: product[] //array of products // coolection of products
+  totalPrice: number
+  // orderStatus: boolean;
+  orderStatus: orderStatus
+  // timestamp: number;
+  timestamp: Date
 };
-type product = {
+type product = { //✅
   productId: number;
   name: string;
   price: number;
   quantity: number;
 };
-type orderStatuses = " pending" | "shipped" | "delivered" | "canceled";
+// type orderStatuses = " pending" | "shipped" | "delivered" | "canceled";
+type orderStatus = " pending" | "shipped" | "delivered" | "canceled";
