ecea8b50e43fd37004d48fbf8842275f6fbcbd11
rehab alzarqa <essamrehab98@gmail.com>
2026-03-15 12:56:47 +0100

    changes

--- app/union-types/page.tsx
+++ app/union-types/page.tsx
@@ -10,22 +10,49 @@ const mont = Montserrat({
 
 
 export default function Home() {
+  // Union types
+  //  OR
+  // first or second or third
 
-    // Union types
-    //  OR
-    // first or second or third
+  // |
+  // first | second | third
 
-    // JSX/TSX starts here
-    return (
-        <div className="w-screen h-screen bg-white flex flex-col items-center justify-center">
-            <div className={`text-center text-[70px] font-black text-transparent bg-clip-text bg-gradient-to-r from-[green] to-[black] ${mont.className}`}>
-                Union Types
-            </div>
-            <div className="text-2xl text-black flex flex-col gap-1">
+  type Light = "on" | "off";
 
-            </div>
+  type Mode = "light" | "dark";
 
-        </div>
-    );
-    // JSX/TSX ends here
+  type Life = "dead" | "living";
+
+  type Mood = "happy" | "sad";
+
+  type Bulb = {
+    light: Light;
+  };
+
+  type Person = {
+    mood: Mood;
+    life: Life;
+    bulb: Bulb;
+  };
+
+  const person: Person = {
+    mood: "happy",
+    life: "living",
+    bulb: {
+        light: "off",
+    },
+  };
+
+  // JSX/TSX starts here
+  return (
+    <div className="w-screen h-screen bg-white flex flex-col items-center justify-center">
+      <div
+        className={`text-center text-[70px] font-black text-transparent bg-clip-text bg-gradient-to-r from-[green] to-[black] ${mont.className}`}
+      >
+        Union Types
+      </div>
+      <div className="text-2xl text-black flex flex-col gap-1"></div>
+    </div>
+  );
+  // JSX/TSX ends here
 }
