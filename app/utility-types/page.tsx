705ab2b871cbb2c92ac4d741f0d5ee6a66aa9c82
rehab alzarqa <essamrehab98@gmail.com>
2026-04-15 10:22:18 +0200

    utility type

--- app/utility-types/page.tsx
+++ app/utility-types/page.tsx
@@ -6,79 +6,102 @@ const mont = Montserrat({
     subsets: ['latin']
 })
 
-let doc: any = {
-
-}
-
 export default function Home() {
+  // Utility Types and they're called like fucntions in Js but with angular braces <>
+  // Partial, Pick, Omit, Record
+  // !not 100%
 
-    // Utility Types and they're called like fucntions in Js but with angular braces <>
-    // Partial, Pick, Omit, Record
-    // !not 100% 
+  type Document = {
+    extension: string;
+    name: string;
+    size: number;
+    pages: number;
+  };
 
-    type Document = {
-        extension: string,
-        name: string,
-        size: number,
-        pages: number
-    }
+  type Author = {
+    name: string;
+    age: number;
+    nationality: string;
+  };
 
+  // !Pick<NameoftheTypeToPickFrom, fieldsYouWantoPick>
+  const student: Pick<Author, "name" | "age"> = {
+    name: "Rehab",
+    age: 12,
+  };
 
+  type Selected = {
+    name: string;
+    age: number;
+  };
 
-    type Author = {
-        name: string,
-        age: number,
-        nationality: string,
-    }
+  const student2: Selected = {
+    name: "Adam",
+    age: 12,
+  };
 
+  const person: Author = {
+    age: 18,
+    name: "Rehab",
+    nationality: "Yemen",
+  };
 
-    // !Pick<NameoftheTypeToPickFrom, fieldsYouWantoPick>
-    const student: Pick<Author, 'name' | 'age'> = {
-        name: 'Rehab',
-        age: 12
-    }
+  // const student: Partial<Author> = {
+  //     name: 'Rehab'
+  // }
+  type Product = {
+    id: number;
+    name: string;
+    price: number;
+  };
 
-    type Selected = {
-        name: string,
-        age: number,
-    }
+  type UpdateProudct = Partial<Product>;
 
-    const student2: Selected = {
-        name: 'Adam',
-        age: 12
-    }
+  type ProductPreview = Pick<Product, "id" | "name">;
 
-    const person: Author = {
-        age: 18,
-        name: 'Rehab',
-        nationality: 'Yemen'
-    }
+  type User = {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+  };
 
-    // const student: Partial<Author> = {
-    //     name: 'Rehab'
-    // }
+  type UserProfile = Partial<Pick<User, "email" | "username">>;
 
+  type Order = {
+    id: number;
+    total: number;
+    status: string;
+    createdAt: Date;
+  };
+  type OrderUpdate = Partial<Pick<Order, "status">>;
 
+  type Profile = {
+    id: number;
+    name: string;
+    bio: string;
+    avatar: string;
+  };
 
+  type ProfileCustom = Pick<Profile, "name"> & Partial<Pick<Profile, "bio">>;
 
+  type Settings = {
+    theme: string;
+    language: string;
+    notifications: boolean;
+  };
 
-
-
-
-
-
-    // JSX/TSX starts here
-    return (
-        <div className="w-screen h-screen bg-white flex flex-col items-center justify-center">
-            <div className={`text-center text-[70px] font-black text-transparent bg-clip-text bg-gradient-to-r from-[green] to-[black] ${mont.className}`}>
-                Utility Types
-            </div>
-            <div className="text-2xl text-black flex flex-col gap-1">
-
-
-            </div>
-
-        </div>
-    );
-    // JSX/TSX ends here
+  type UpdateSettings = Partial<Pick<Settings, "theme" | "language">>;
+  // JSX/TSX starts here
+  return (
+    <div className="w-screen h-screen bg-white flex flex-col items-center justify-center">
+      <div
+        className={`text-center text-[70px] font-black text-transparent bg-clip-text bg-gradient-to-r from-[green] to-[black] ${mont.className}`}
+      >
+        Utility Types
+      </div>
+      <div className="text-2xl text-black flex flex-col gap-1"></div>
+    </div>
+  );
+  // JSX/TSX ends here
 }
